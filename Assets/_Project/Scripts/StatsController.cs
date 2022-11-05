e58c19c9a15c17412676e214ff90aa7f7d1c44fb
Baron Drakula <karyol.kj@gmail.com>
2022-11-05 15:55:21 +0100

    PGK-11 Mana and stamina regen

--- Assets/_Project/Scripts/StatsController.cs
+++ Assets/_Project/Scripts/StatsController.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 public class StatsController : MonoBehaviour
 {
     public int health;
+    public int maxHealth;
     public int mana;
+    public int maxMana;
+    public int manaRegen;
     public int stamina;
+    public int maxStamina;
+    public int staminaRegen;
     public int attack;
     public int deffense;
     public float movementSpeed;
@@ -15,6 +20,9 @@ public class StatsController : MonoBehaviour
     public float climbingSpeed;
     public float dodgingTime;
     public float dodgeCooldown;
+
+    private bool regeneratingMana = false;
+    private bool regeneratingStamina = false;
 
     // Start is called before the first frame update
     void Start()
@@ -25,6 +33,36 @@ public class StatsController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(mana < maxMana && !regeneratingMana)
+        {
+            InvokeRepeating(nameof(regenerateMana), 0f, 1f / manaRegen);
+        }
+
+        if(stamina < maxStamina && !regeneratingStamina)
+        {
+            InvokeRepeating(nameof(regenerateStamina), 0f, 1f / staminaRegen);
+        }
+    }
+
+    private void regenerateMana()
+    {
+        regeneratingMana = true;
+        mana++;
+        if(mana == maxMana)
+        {
+            regeneratingMana = false;
+            CancelInvoke(nameof(regenerateMana));
+        }
+    }
+
+    private void regenerateStamina()
+    {
+        regeneratingStamina = true;
+        stamina++;
+        if(stamina == maxStamina)
+        {
+            regeneratingStamina = false;
+            CancelInvoke(nameof(regenerateStamina));
+        }
     }
 }
