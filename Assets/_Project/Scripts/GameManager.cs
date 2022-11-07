228ebfc6e50c3db7ecaa0a7cfd8f3e84bff301d7
Baron Drakula <karyol.kj@gmail.com>
2022-11-07 12:31:39 +0100

    PGK-17 Created game manager that wont destroy on scene load

---
+++ Assets/_Project/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
